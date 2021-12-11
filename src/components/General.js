ee08d6eb4cb9f24e3517b8f6a4d0e1da80622704
Rhyine <rhyinestewart@gmail.com>
2021-12-11 13:46:42 -0500

    Add initial edit for each general element

--- src/components/General.js
+++ src/components/General.js
@@ -7,16 +7,7 @@ class General extends React.Component {
     this.state = {}
   }
   render() {
-    if (!this.props.general.edit) {
-      return (
-        <div className='GeneralContainer'>
-          <h2 className='Name' onClick={this.props.handleClick}>{this.props.general.full_name}</h2>
-          <h3 className='Role' onClick={this.props.handleClick}>{`${this.props.general.current_role}`}</h3>
-          <p className='Contact' onClick={this.props.handleClick}>{`${this.props.general.email} | ${this.props.general.phone}`}</p>
-        </div>
-      )
-    }
-    if (this.props.general.edit === this.props.general.full_name){
+    if (this.props.general.edit === this.props.general.full_name) {
       return (
         <div className='GeneralContainer'>
           <div className='NameInput'>
@@ -24,18 +15,39 @@ class General extends React.Component {
           </div>
           <h3 className='Role' onClick={this.props.handleClick}>{`${this.props.general.current_role}`}</h3>
           <p className='Contact' onClick={this.props.handleClick}>{`${this.props.general.email} | ${this.props.general.phone}`}</p>
-          {/* <div className='RoleInput'>
-            <input value={this.props.general.current_role} />
-          </div>
-          <div className='ContactInput'>
-            <input value={this.props.general.email} />
-            <input value={this.props.general.phone} />
-          </div> */}
         </div>
       )
     }
-    return(
-      <div>Hello</div>
+    if (this.props.general.edit === this.props.general.current_role) {
+      return (
+        <div className='GeneralContainer'>
+          <h2 className='Name' onClick={this.props.handleClick}>{this.props.general.full_name}</h2>
+          <div className='RoleInput'>
+            <input value={this.props.general.current_role} />
+          </div>
+          <p className='Contact' onClick={this.props.handleClick}>{`${this.props.general.email} | ${this.props.general.phone}`}</p>
+        </div>
+      )
+    }
+
+    if (this.props.general.edit === `${this.props.general.email} | ${this.props.general.phone}`) {
+      return (
+        <div className='GeneralContainer'>
+          <h2 className='Name' onClick={this.props.handleClick}>{this.props.general.full_name}</h2>
+          <h3 className='Role' onClick={this.props.handleClick}>{`${this.props.general.current_role}`}</h3>
+          <div className='ContactInput'>
+            <input value={this.props.general.email} />
+            <input value={this.props.general.phone} />
+          </div>
+        </div>
+      )
+    }
+    return (
+      <div className='GeneralContainer'>
+        <h2 className='Name' onClick={this.props.handleClick}>{this.props.general.full_name}</h2>
+        <h3 className='Role' onClick={this.props.handleClick}>{`${this.props.general.current_role}`}</h3>
+        <p className='Contact' onClick={this.props.handleClick}>{`${this.props.general.email} | ${this.props.general.phone}`}</p>
+      </div>
     )
   }
 }
