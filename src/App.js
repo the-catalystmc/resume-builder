0fbeef2ac1a5d05bfef04f16bb1a36c21efd74f4
Rhyine <rhyinestewart@gmail.com>
2021-12-11 12:32:32 -0500

    Add state and basic elements for layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import General from './components/General';
 import './App.css';
 class App extends React.Component {
     constructor() {
         super();
     
         this.state = {
-          task: {
-            text: '',
-            id: uuidv4(),
-            num: 1,
-          },
-          tasks: [],
-          edit: false,
-        };
-    
+            general: {
+                full_name: 'Rhyine Stewart',
+                current_role: 'DEVELOPER',
+                phone: '1 876 390-5206',
+                email: 'rhyinestewart@gmail.com',
+                linkedin: 'linkedin.com/rhyine-stewart'
+            },
+        }
+        this.handleClick = this.handleClick.bind(this);
         // Bind eventhandler functions
         // this.inputChange = this.inputChange.bind(this);
         // this.submitClicked = this.submitClicked.bind(this);
         // this.deleteClicked = this.deleteClicked.bind(this);
         // this.updateText = this.updateText.bind(this);
       }
+
+      handleClick = (e) => {
+        console.log('Element Clicked')
+      }
+
     render() {
         return (
+            <div className='App-Cont'>
             <div className="App">
-
+                <General general={this.state.general} handleClick={this.handleClick} />
+            </div>
             </div>
         );
     }
