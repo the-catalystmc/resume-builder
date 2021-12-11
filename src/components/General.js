b86d387d3f6d2bda384eab7d567612123657fbd8
Rhyine <rhyinestewart@gmail.com>
2021-12-11 12:32:59 -0500

    Create General component and add layout

--- src/components/General.js
+++ src/components/General.js
@@ -1,1 +1,25 @@
 //name, email, phone number
+import React from 'react';
+
+class General extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className='GeneralContainer'>
+                <h2 className='Name' onClick={this.props.handleClick}>{this.props.general.full_name}</h2>
+                <h3 className='Role'>{`${this.props.general.current_role}`}</h3>
+                <p className='Contact'>{`${this.props.general.email} | ${this.props.general.phone}`}</p>
+                {/* <div className='ContactInfo'>
+                    <p className='Email'>{this.props.general.first_name}</p>
+                    <p className='LinkedIn'>{this.props.general.first_name}</p>
+                    <p className='Phone'>{this.props.general.first_name}</p>
+                </div> */}
+            </div>
+        )
+    }
+}
+
+export default General;
