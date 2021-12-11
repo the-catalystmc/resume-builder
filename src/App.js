5a08e78abf0b709f6b8f8eb00cf26abc42abc100
Rhyine <rhyinestewart@gmail.com>
2021-12-11 10:24:42 -0500

    Install uuidv4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 class App extends React.Component {
-
+    constructor() {
+        super();
+    
+        this.state = {
+          task: {
+            text: '',
+            id: uuidv4(),
+            num: 1,
+          },
+          tasks: [],
+          edit: false,
+        };
+    
+        // Bind eventhandler functions
+        this.inputChange = this.inputChange.bind(this);
+        this.submitClicked = this.submitClicked.bind(this);
+        this.deleteClicked = this.deleteClicked.bind(this);
+        this.updateText = this.updateText.bind(this);
+      }
     render() {
         return (
             <div className="App">
