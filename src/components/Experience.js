b57cf1590c9372a065df0254b2ec266cfcd91a1d
Rhyine <rhyinestewart@gmail.com>
2021-12-19 23:00:58 -0500

    Add experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,1 +1,26 @@
 //company name, position title, main tasks of your jobs, date from and until when you worked for that company
+import React from 'react'
+
+class Experience extends React.Component {
+    render() {
+        return (
+            <div>
+                <hr />
+                <h4>EXPERIENCE</h4>
+                <hr />
+                <div className='ExpCont'>
+                    <div>
+                        <p>COMPANY</p>
+                        <p><b>Position Title</b></p>
+                    </div>
+                    <div className='ExpLocation'>
+                        <p>Location</p>
+                        <p>Time Frame</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Experience;
